608668af996ae6d5e2c07a98c4df567e577ee1fb
Balu Chitturi <137925242+BaluChitturi@users.noreply.github.com>
2024-08-21 23:59:56 +0530

    Update ListProduct.jsx

--- e-commerce-admin/src/Components/ListProduct/ListProduct.jsx
+++ e-commerce-admin/src/Components/ListProduct/ListProduct.jsx
@@ -6,7 +6,7 @@ const ListProduct = () => {
   const [allproducts, setAllProducts] = useState([]);
 
   const fetchInfo = () => { 
-    fetch('https://ecommerce-backend-w15r.onrender.com/allproducts') 
+    fetch('https://ecommerce-backend-z6lf.onrender.com/allproducts') 
             .then((res) => res.json()) 
             .then((data) => setAllProducts(data))
     }
@@ -16,7 +16,7 @@ const ListProduct = () => {
     }, [])
 
     const removeProduct = async (id) => {
-      await fetch('https://ecommerce-backend-w15r.onrender.com/removeproduct', {
+      await fetch('https://ecommerce-backend-z6lf.onrender.com/removeproduct', {
       method: 'POST',
       headers: {
         Accept:'application/json',
@@ -25,7 +25,7 @@ const ListProduct = () => {
       body: JSON.stringify({id:id}),
     })
 
-    fetch('https://ecommerce-backend-w15r.onrender.com/allproducts') 
+    fetch('https://ecommerce-backend-z6lf.onrender.com/allproducts') 
     .then((res) => res.json()) 
     .then((data) => setAllProducts(data))
 
